2b7d566a4c7a19e3e9483ed8de3771b2be6e8dd5
Baibhavi <baibhavipandey@gmail.com>
2023-09-14 08:19:23 +0530

    some more questions

--- Q3.java
+++ Q3.java
@@ -3,7 +3,17 @@ import java.util.Scanner;
 public class Q3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String n = sc.next();
-        
+        char[] n = sc.next().toCharArray();
+        for(int i = 0; i < n.length; i++){
+            if(n[i]>'4'){
+                n[i] = (char)('9' - n[i] + '0');
+            }
+            if(n[0]=='0'){
+                n[0]='9';
+            }
+        }
+        System.out.println(n);
+        System.out.println('9'-'2'+'0');
+        sc.close();
     }
 }
